b69217628c5acb207471e9e222092ce3e8e1a16d
Francisco Felipe <francisco@attivilog.com.br>
2021-02-02 23:06:33 -0300

    Upgrading dependencies and creating some new models

---
+++ src/models/Collection.js
@@ -0,0 +1,13 @@
+import { Model } from 'sequelize';
+
+export default class Collection extends Model {
+  static init(sequelize, DataTypes) {
+    return super.init(
+      {
+        name: { type: DataTypes.STRING },
+        permissionType: { type: DataTypes.STRING },
+      },
+      { tableName: 'collection', sequelize, underscored: true },
+    );
+  }
+}
